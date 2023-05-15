a51f5a261c7692eede1f6e0ce556805087ae452b
George <gbemidale.a@gmail.com>
2023-05-15 10:46:04 +0100

    new additions

--- AsmtLib/ClassLib.cs
+++ AsmtLib/ClassLib.cs
@@ -9,11 +9,16 @@ namespace AsmtLib
         {
             int num = GetInputNum();
 
-            var numArray = GetNumList(num);
-
-            foreach (int item in numArray)
+            if (num == 0)
+                Console.WriteLine("ZERO");
+            else
             {
-                Console.WriteLine(item);
+                var numArray = GetNumArray(num);
+
+                foreach (int item in numArray)
+                {
+                    Console.WriteLine(item);
+                }
             }
         }
 
@@ -38,7 +43,7 @@ namespace AsmtLib
             return intInput;
         }
 
-        private static int[] GetNumList(int num)
+        private static int[] GetNumArray(int num)
         {
             List<int> numList = new List<int>();
 
@@ -51,11 +56,30 @@ namespace AsmtLib
             return numList.ToArray();
         }
 
-        private static void FirstLevelTxt()
+        private static void FirstLevelText(int[] numArray)
         {
+            if (numArray.Length > 3)
+            {
+                Console.WriteLine("ERROR!! INVALID INPUT");
+            }
+            else
+            {
+                Dictionary<int, string> firstStage = new Dictionary<int, string>()
+                {
+                    {1, "ONE"}, {2, "TWO"}, {3, "THREE"}, {4, "FOUR"}, {5, "FIVE"},
+                    {6, "SIX"}, {7, "SEVEN"}, {8, "EIGHT"}, {9, "NINE"}, {10, "TEN"},
+                    {11, "ELEVEN"}, {12, "TWELVE"}, {13, "THIRTEEN"}, {14, "FOURTEEN"}, {15, "FIFTEEN"},
+                    {16, "SIXTEEN"}, {17, "SEVENTEEN"}, {18, "EIGHTEEN"}, {19, "NINETEEN"}
+                };
 
+                Dictionary<int, string> secondStage = new Dictionary<int, string>()
+                {
+                    {20, "TWENTY"}, {30, "THIRTY"}, {40, "FORTY"}, {50, "FIFTY"}, {60, "SIXTY"},
+                    {70, "SEVENTY"}, {80, "EIGHTY"}, {90, "NINTY"}
+                };
+            }
         }
 
-        private static void SecondLevelTxt() { } //input and return values not yet determined
+        private static void SecondLevelText() { } //input and return values not yet determined
     }
 }
